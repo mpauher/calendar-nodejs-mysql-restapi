2236b98c5923cf9c401adaf1f6659a23939baedd
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-01-31 10:12:33 -0500

    SOme changes on index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import eventsRoutes from './routes/events.routes.js';
 import participantsRoutes from './routes/participants.routes.js'
+import filterRoutes from './routes/filter.routes.js'
 
 const app = express();
 
+app.use(express.json())
+
 app.use('/api',eventsRoutes)
 app.use('/api',participantsRoutes)
+app.use('/api',filterRoutes)
 
 
-app.listen(9000);
 
-console.log('Server running on port 9000');
+app.listen(7000);
+
+console.log('Server running on port 7000');
